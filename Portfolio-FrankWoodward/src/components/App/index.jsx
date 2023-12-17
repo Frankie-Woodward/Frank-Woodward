1ba3358cf1674e3c01bf5bf20e5cfbf46bc81333
Frankie <fwoodwar@gmail.com>
2023-12-17 09:59:45 -0700

    initialized react project for Frankie Portfolio. Created dependencies and components.

---
+++ Portfolio-FrankWoodward/src/components/App/index.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './style.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from '../HomePage';
+import ProjectsPage from '../ProjectsPage';
+import FrankiePage from '../FrankiePage';
+import ContactPage from '../ContactPage';
+
+export default function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
