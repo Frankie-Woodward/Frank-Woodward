61857a8c2b32b7e677f8c028d79f992806c64386
Frankie <fwoodwar@gmail.com>
2024-01-02 21:07:44 -0700

    added logos for the different frameworks and languages I am trained in. Also customized languages section with css changes

--- Portfolio-FrankWoodward/src/components/HomePage/index.jsx
+++ Portfolio-FrankWoodward/src/components/HomePage/index.jsx
@@ -1,6 +1,15 @@
 import './style.css'
 import React from 'react';
 import arrowImage from '../../assets/arrow.jpg';
+import bootstrap_logo from '../../assets/bootstrap-logo.svg';
+import CSS3_logo from '../../assets/CSS3 - logo .png';
+import express_js_logo from '../../assets/Expressjs-logo.svg';
+import HTML5_logo from '../../assets/html5-logo.png';
+import javaScript_logo from '../../assets/JavaScript-Logo.png';
+import mongoDB_logo from '../../assets/MongoDB-Logo.jpg';
+import python_logo from '../../assets/python-logo.png';
+import react_logo from '../../assets/react-logo.png';
+import postgresql_logo from '../../assets/postgresql-logo.png';
 import profile_pic from '../../assets/profile_picture.jpg';
 import FrankieChatBot from '../AIFrankieBot';
 export default function Homepage() {
@@ -44,15 +53,39 @@ const ArrowAnimation = () => {
                     <h2>Programming Languages</h2>
                     <ul className="languages-list">
                         <li className="language-item">
-                            <img className="language-icon" src="path_to_js_icon" alt="JavaScript" />
+                            <img className="language-icon" src={javaScript_logo} alt="JavaScript" style={{ width: '50px', height: '50px' }} />
                             <span>JavaScript</span>
                         </li>
                         <li className="language-item">
-                            <img className="language-icon" src="path_to_python_icon" alt="Python" />
+                            <img className="language-icon" src={python_logo} alt="Python" />
                             <span>Python</span>
                         </li>
                         <li className="language-item">
-                            <img className="language-icon" src="path_to_css_icon" alt="CSS" />
+                            <img className="language-icon" src={CSS3_logo} alt="CSS" />
+                            <span>CSS</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={express_js_logo} alt="express.js" />
+                            <span>Express.js</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={HTML5_logo} alt="HTML5" />
+                            <span>HTML5</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={mongoDB_logo} alt="mongodb" />
+                            <span>MongoDB</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={postgresql_logo} alt="postgresql" />
+                            <span>Postgresql</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={react_logo} alt="react" />
+                            <span>React</span>
+                        </li>
+                        <li className="language-item">
+                            <img className="language-icon" src={CSS3_logo} alt="CSS" />
                             <span>CSS</span>
                         </li>
                         {/* ... add more as needed */}
